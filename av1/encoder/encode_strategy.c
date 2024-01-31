04bf7ac5d7db93b1fbca3f35b1d6ec32dfa35393
Damian Höster <damian.hoester@posteo.de>
2024-01-31 13:56:45 +0100

    Fix vmaf-preprocessing 1-2
    Now they don't need vmaf-quantization=1 parameter to be active.

--- av1/encoder/encode_strategy.c
+++ av1/encoder/encode_strategy.c
@@ -1732,7 +1732,8 @@ int av1_encode_strategy(AV1_COMP *const cpi, size_t *const size,
   if (!is_stat_generation_stage(cpi) &&
       ((oxcf->tune_cfg.tuning >= AOM_TUNE_VMAF_WITH_PREPROCESSING &&
        oxcf->tune_cfg.tuning <= AOM_TUNE_VMAF_NEG_MAX_GAIN) ||
-       oxcf->vmaf_quantization == 1)) {
+       oxcf->vmaf_quantization == 1 ||
+       oxcf->vmaf_preprocessing == 1 || oxcf->vmaf_preprocessing == 2)) {
     av1_update_vmaf_curve(cpi);
   }
 #endif
