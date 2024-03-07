993603ae5a491f91ee9d6d4146651fe0965e0785
Damian Höster <damian.hoester@posteo.de>
2024-03-07 20:56:52 +0100

    Revert "Change default dist-metric to qm-psnr"
    This reverts commit 3fe109119e564e50a1b6faeeb5cd060874f47991.

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -249,7 +249,7 @@ static const struct av1_extracfg default_extra_cfg = {
   ".",                                        // partition info path
   0,                                          // enable rate guide deltaq
   "./rate_map.txt",                           // rate distribution input
-  AOM_DIST_METRIC_QM_PSNR,                    // dist_metric
+  AOM_DIST_METRIC_PSNR,                       // dist_metric
   10,                                         // cq_level
   0,                                          // rc_max_intra_bitrate_pct
   0,                                          // rc_max_inter_bitrate_pct
@@ -407,7 +407,7 @@ static const struct av1_extracfg default_extra_cfg = {
   ".",                                        // partition info path
   0,                                          // enable rate guide deltaq
   "./rate_map.txt",                           // rate distribution input
-  AOM_DIST_METRIC_QM_PSNR,                    // dist_metric
+  AOM_DIST_METRIC_PSNR,                       // dist_metric
   10,                                         // cq_level
   0,                                          // rc_max_intra_bitrate_pct
   0,                                          // rc_max_inter_bitrate_pct
