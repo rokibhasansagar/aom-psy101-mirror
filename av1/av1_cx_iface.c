37603f1d48fdf7a510ec28091fb2be82192b03eb
Damian Höster <damian.hoester@posteo.de>
2024-11-11 15:52:26 +0100

    Force deltaq-mode 2 for tune-content psy101 also in allintra mode

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -1319,7 +1319,8 @@ static void set_encoder_config(AV1EncoderConfig *oxcf,
   q_cfg->quant_b_adapt = extra_cfg->quant_b_adapt;
   q_cfg->enable_chroma_deltaq = extra_cfg->enable_chroma_deltaq;
   q_cfg->aq_mode = extra_cfg->aq_mode;
-  if (oxcf->passes == 2 && tune_cfg->content == AOM_CONTENT_PSY101) {
+  if ((oxcf->passes == 2 || oxcf->mode == ALLINTRA) &&
+      tune_cfg->content == AOM_CONTENT_PSY101) {
     q_cfg->deltaq_mode = DELTA_Q_PERCEPTUAL;
   } else {
     q_cfg->deltaq_mode = extra_cfg->deltaq_mode;
